3233173d18f2ac8baa47605d8055e0d733ca6a2d
rrivera <rrivera@redbannermedia.com>
2024-06-17 19:48:58 -0400

    created category page and product page

--- plugins/rbm/stripe/controllers/product/index.php
+++ plugins/rbm/stripe/controllers/product/index.php
@@ -1,43 +1,40 @@
-<main class="fs-4">
+<main class="fs-4 my-3 d-flex flex-column">
     <h1>Stripe Product Configuration</h1>
     <p>
         Add products here to create products for your store.
     </p>
-    <form>
-        <div>
+    <form class="w-25">
+        <div class="my-3 d-flex flex-column">
             <label for="name">Name</label>
             <input type="text" name="name" />
         </div>
-        <div>
+        <div class="my-3 d-flex flex-column">
             <label for="featured_text">Featured Text</label>
             <textarea name="feature_text"></textarea>
         </div>
-        <div>
+        <div class="my-3 d-flex flex-column">
             <label for="price">Price</label>
             <input type="number" name="price" />
         </div>
-        <div>
+        <div class="my-3 d-flex flex-column">
             <label for="stock">Stock</label>
             <input type="range" name="stock" />
         </div>
-        <div>
+        <div class="my-3 d-flex flex-row">
             <label for="featured">Featured Item</label>
             <input type="checkbox" name="featured" />
         </div>
-        <div>
-            <label for="product_image">Product Image</label>
-            <input type="file" name="product_image" />
-        </div>
-        <div>
+        <div class="my-3 d-flex flex-column">
             <label for="product_image">Product Image</label>
             <input type="file" name="product_image" />
         </div>
         <!--TODO: Pull from categories from plugin-->
-        <div>
-            <label for="category">Product Image</label>
+        <div class="my-3 d-flex flex-column">
+            <label for="category">Product Category</label>
             <select name="category">
                 <option value="">--Please Choose a Category</option>
             </select>
         </div>
+        <input type="submit" value="Add Product" class="bg-success border rounded" />
     </form>
 </main>
